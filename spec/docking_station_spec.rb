fe2673f0515775cbb725b788a376aca875d10ec8
Arif Ebrahim <>
2021-04-27 17:41:26 +0100

    passed first feature test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,9 @@ require 'docking_station'
 
 describe DockingStation do 
   it {is_expected.to respond_to (:release_bike)}
-end
 
-describe DockingStation do
-  DockingStation.release_bike 
-  it {is_expected.to respond_to :working?}
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike.working?).to be true
+  end
 end
